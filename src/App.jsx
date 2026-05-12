0eac57e52c8ede893de04a58c1ffa662eff8f52e
“Nilde <nilde.benedetti@gmail.com>
2026-05-12 15:07:03 +0200

    add: import con re-picking per elementi libreria react-router + pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
-
+{/* import con repicking  per elementi libreria */}
+import { BrowserRouter, Rooutes, Route } from "react-router"; 
+import Home from "./pages/Home";
+import ChiSiamo from "./pages/ChiSiamo";
+import Prodotti from "./pages/Prodotti";
 
 function App() {
   return <div>
