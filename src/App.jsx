7fc0dd8d454009d3ca390a5952ac90e856d050d5
“Nilde <nilde.benedetti@gmail.com>
2026-05-12 16:24:55 +0200

    add: pagina NotFound + Route ssoluta

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import Home from "./pages/Home";
 import ChiSiamo from "./pages/ChiSiamo";
 import Prodotti from "./pages/Prodotti";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -12,7 +13,12 @@ function App() {
 
       <Route path="/chi-siamo" Component={ChiSiamo} />
 
-      <Route path="/Prodotti" Component={Prodotti} />
+      <Route path="/prodotti" Component={Prodotti} />
+
+      {/* Inserisco la route per la pgina errore 
+      di modo che se fallisce la ricerca a cascata 
+      mostri la pag di errore 404 */}
+      <Route path="*" Component={NotFound} />
     </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,28 @@
+import { Link, NavLink} from "react-router"
+
+function NotFound() {
+    return <div className="container vh-100 d-flex align-items-center justify-content-center">
+        <div className="text-center">
+            {/* Icona o Numero Errore */}
+            <h1 className="display-1 fw-bold text-primary">404</h1>
+
+            <h2 className="mb-3">Oops! Pagina non trovata</h2>
+
+            <p className="lead mb-4 text-secondary">
+                Sembra che il capo che stavi cercando sia andato esaurito o che l'indirizzo sia errato.
+            </p>
+
+            {/* Bottone di ritorno alla Home */}
+            <Link to="/" className="btn btn-dark btn-lg px-5 shadow-sm">
+                Torna alla Home
+            </Link>
+
+            <div className="mt-5">
+                <small className="text-muted">
+                    Se pensi sia un errore del sistema, contatta il supporto di **LastDrop**.
+                </small>
+            </div>
+        </div>
+    </div>
+}
+export default NotFound
