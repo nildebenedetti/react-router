33a82b21f01aba3e9a6665ac8df39f2c859c2bd3
“Nilde <nilde.benedetti@gmail.com>
2026-05-12 15:51:08 +0200

    add: li dell navbar con nomi category pages + dimensioni logo

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+function Navbar() {
+    return <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+                <img src="./images/lastdrop-logo-1.png" alt="lastdrop logo" className="navbar-logo"></img>
+            </a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Prodotti</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Chi Siamo</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+}
+export default Navbar
