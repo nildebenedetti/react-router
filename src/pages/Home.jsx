3c27b6f8782f9eaf280c05b15fd9422bf3d8ff7d
“Nilde <nilde.benedetti@gmail.com>
2026-05-12 18:29:09 +0200

    add: hero banner come primo elemento + style per i banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,9 @@ import Navbar from "../components/Navbar";
 function Home() {
     return <div>
         <Navbar />
-        ciao sono la home
+        <div className="hero-container">
+            <img className="hero-banner" src="./images/hero-banner.png" alt="home interior con uomo che guarda un paesaggio verde dalla vetrata"></img>
+        </div>
     </div>
 }
 export default Home
