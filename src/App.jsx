9eba5cae1b62dea14ff8ffe717df08fbdb3d7e55
“Nilde <nilde.benedetti@gmail.com>
2026-05-12 15:46:42 +0200

    fix typo in import - App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 {/* import con repicking  per elementi libreria */}
-import { BrowserRouter, Rooutes, Route } from "react-router"; 
+import { BrowserRouter, Routes, Route } from "react-router"; 
 import Home from "./pages/Home";
 import ChiSiamo from "./pages/ChiSiamo";
 import Prodotti from "./pages/Prodotti";
