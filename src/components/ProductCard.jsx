496a927c35c263d9f0d993bac90600ba7e89c221
“Nilde <nilde.benedetti@gmail.com>
2026-05-12 17:49:32 +0200

    fix: layout delle card

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -5,10 +5,10 @@ function ProductCard({
     productCategory = "",
     productDescription = "A beautiful product you definitely need. Handmade by fairies."
 }) {
-    return <div className="card col-12 col-md-3">
+    return <div className="card col-12 col-md-6 col-lg-4 p-3 m-3 d-flex flex-column align-items-center justify-content-center">
+        <div><h5 className="card-title productName py-3">{productName}</h5></div>
         <img src={productImg} className="card-img-top" alt={productName}></img>
         <div className="card-body">
-            <h5 className="card-title productName">{productName}</h5>
             <p className="card-text productDescription">{productDescription}</p>
             <p className="card-text productPrice">
                 <span>&euro; </span>{productPrice}
