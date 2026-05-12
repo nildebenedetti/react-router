846462b6c3627a8b3f50baff4e6804195353aa18
“Nilde <nilde.benedetti@gmail.com>
2026-05-12 16:39:15 +0200

    add: jsx della pagina prodotti

--- src/pages/Prodotti.jsx
+++ src/pages/Prodotti.jsx
@@ -1,13 +1,25 @@
 import { Link, NavLink } from "react-router";
 import Navbar from "../components/Navbar";
+import ProductCard from "../components/ProductCard";
 
 
 function Prodotti() {
-  return (
-    <div>
-        <Navbar/>
-        <p>Prodotti</p>
+    return <div>
+        <Navbar />
+        <header className="header d-flex justify-content-center py-3 flex-column">
+            <div className="px-3">
+                <h3> Prodotti</h3>
+            </div>
+            <div className="px-3">
+                <p>Trova l'articolo perfetto per te!</p>
+            </div>
+        </header>
+        <div className="container d-flex justify-content-center mx-3">
+            <div className="row d-flex justify-content-center align-items-center">
+
+            </div>
         </div>
-  )
+
+    </div>
 }
 export default Prodotti
