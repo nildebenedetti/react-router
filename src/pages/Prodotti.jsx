a91015f9cb92280207f5a786e7dc30af7ae04064
“Nilde <nilde.benedetti@gmail.com>
2026-05-12 17:30:10 +0200

    add: product list con ProductCard component + map nella pagina Products.jsx

--- src/pages/Prodotti.jsx
+++ src/pages/Prodotti.jsx
@@ -10,46 +10,59 @@ function Prodotti() {
     di modo che sia dinamica e aggiornata quando navigo questa pagina
     e gestire eventuali trigger che verranno aggiunti in futuro*/}
 
-    const [ productList, setProductList ] = useState([]);
+    const [productList, setProductList] = useState([]);
 
     {/* adesso devo scrivermi la fetch dei prodotti */ }
-    useEffect (() => {
+    useEffect(() => {
         const URL_API = 'https://fakestoreapi.com/products';
 
         fetch(URL_API)
-        .then ( result => {
-            return result.json();
-        })
-        .then ( data => {
-            setProductList(data);
-        })
-        .catch ( error => {
-            console.error('errore durnte importazione dati',error)
-        })
+            .then(result => {
+                return result.json();
+            })
+            .then(data => {
+                setProductList(data);
+                console.log(productList);
+
+            })
+            .catch(error => {
+                console.error('errore durnte importazione dati', error)
+            })
 
     }, []);
 
 
 
-return <div>
-    <Navbar />
-    <header className="header d-flex justify-content-center py-3 flex-column">
-        <div className="px-3">
-            <h3> Prodotti</h3>
+    return <div>
+        <Navbar />
+        <header className="header d-flex justify-content-center py-3 flex-column">
+            <div className="px-3">
+                <h3> Prodotti</h3>
+            </div>
+            <div className="px-3">
+                <p>Trova l'articolo perfetto per te!</p>
+            </div>
+        </header>
+        <div className="container d-flex justify-content-center mx-3">
+            <div className="row d-flex justify-content-center align-items-center">
+                <div>
+                    <ul>{productList.map(product => {
+                        return <li key={product.id}>
+                            <ProductCard
+                                productImg={product.image}
+                                productName={product.title}
+                                productPrice={product.price}
+                                productCategory={product.category}
+                                productDescription={product.description}
+                            />
+                        </li>
+                    })}
+                    </ul>
+                </div>
+            </div>
         </div>
-        <div className="px-3">
-            <p>Trova l'articolo perfetto per te!</p>
-        </div>
-    </header>
-    <div className="container d-flex justify-content-center mx-3">
-        <div className="row d-flex justify-content-center align-items-center">
-        <div>
-            {JSON.stringify(productList)}
-        </div>
-        </div>
+
+
     </div>
-
-
-</div>
 }
 export default Prodotti
