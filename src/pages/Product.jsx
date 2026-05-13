c83b357e66f2873a93a0051ffaed6598b9c6e8ac
“Nilde <nilde.benedetti@gmail.com>
2026-05-13 17:20:17 +0200

    creato la fetch per recuperare i dati del singolo prodotto

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,21 +1,42 @@
 import {
+    Link,
+    Navigate,
+    UNSAFE_FetchersContext,
+    useNavigate,
     useParams
 } from "react-router";
 
+import { useState } from "react";
+import { useEffect } from "react";
+import ProductCard from "../components/ProductCard";
+
 function Product() {
-    const { productId } = useParms() {/* ma come glielo passo sto qua? */}
+    const { id } = useParams() /* come se gli stessimo segnalando
+        che per il componente products deve recuperare dalla route il parametro dinamico */
+    const [ product, setProduct ] = useState({});
+    const API_URL = 'https://fakestoreapi.com/products';
 
-    
+    useEffect(() => {
+        fetch(`${API_URL}/${id}`)
+            .then(result => {
+                return result.json();
+            })
+            .then(data => {
+                setProduct(data);
+                console.log(product);
 
-  return <>
-<h1> sono un product</h1>
-  {/* io devo fare un nuovo fetchone di un singolo prodotto
-  nella doc di fakestoreAPI
-  function fetchSIngleProductData(ProductId) {
-    return fetch(`${API_URL}/${ProductId}`)
-  } va messa dentro l use effecte nellárray delle dipendenze ci mettero productId, cosi ogni volta che cambia il singolo prosdotto viene rifatta
-  */}
-  
-  </>
+            })
+            .catch( error => {
+                console.error('errore nel caricamento dei dati',error);
+                
+            })
+    }, [id]);
+
+    return <>
+        <div className="container">
+        <p>{JSON.stringify(product)}</p>
+        </div>
+
+    </>
 }
 export default Product
