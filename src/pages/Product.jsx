764e26ad28627d310461a10d8df84d97e574372c
“Nilde <nilde.benedetti@gmail.com>
2026-05-13 18:04:04 +0200

    add: layout PDP (POCO AMBIZIOSO)

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,7 +1,6 @@
 import {
     Link,
     Navigate,
-    UNSAFE_FetchersContext,
     useNavigate,
     useParams
 } from "react-router";
@@ -13,8 +12,10 @@ import ProductCard from "../components/ProductCard";
 function Product() {
     const { id } = useParams() /* come se gli stessimo segnalando
         che per il componente products deve recuperare dalla route il parametro dinamico */
-    const [ product, setProduct ] = useState({});
+    const [product, setProduct] = useState({});
     const API_URL = 'https://fakestoreapi.com/products';
+    const { title, description, category, image, price } = product;
+
 
     useEffect(() => {
         fetch(`${API_URL}/${id}`)
@@ -26,15 +27,34 @@ function Product() {
                 console.log(product);
 
             })
-            .catch( error => {
-                console.error('errore nel caricamento dei dati',error);
-                
+            .catch(error => {
+                console.error('errore nel caricamento dei dati', error);
+
             })
     }, [id]);
 
     return <>
-        <div className="container">
-        <p>{JSON.stringify(product)}</p>
+        <div className="container pdp-container">
+            <div class="row">
+                {/** il primo blocco con titolo - rating - img main  */}
+                <div class="col-12 col-lg-8 d-flex flex-column justify-content-center">
+                    <h1>{title}</h1>
+                    <div className="rating">
+                       {/* <p className="rating-p">{`${rate}/5 based on ${count} global contributors`}</p>*/}
+                    </div>
+                    <div className="container object-fit-cover">
+                        <img src={image} className="pdp-main-img" alt={title}></img>
+                    </div>
+                    <p className="pdp-price-text fw-bold">
+                        <span>&euro; </span>{price}
+                    </p>
+                </div>
+                {/** il secondo blocco con numero x in categoria - descrizione   */}
+                <div class="col-12 col-lg-4 d-flex flex-column justify-content-center">
+                <p className="pdp-text fw-bold score-in-category">{`#${id} rated in ${category}`}</p>
+                <p className="pdp-text pdp-description">{description}</p>
+                </div>
+            </div>
         </div>
 
     </>
